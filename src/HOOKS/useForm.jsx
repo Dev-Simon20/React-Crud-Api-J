6db7850b36ee4cd92828e0e90639cd5366fa7882
Dev-Simon20 <jeanpierks6@gmail.com>
2023-11-24 19:45:51 -0500

    Maneja los cambios en los inmputs del formulario

--- src/HOOKS/useForm.jsx
+++ src/HOOKS/useForm.jsx
@@ -1,14 +1,26 @@
 import { useEffect, useState } from "react"
 
-export const useForm=(initialForm)=>{
+export const useForm=(initialForm,validateForm)=>{
   const [form,setForm]=useState(initialForm);
   const [error,setError]=useState({});
   const [loading,setLoading]=useState(false);
   const [response,setResponse]=useState(null)
 
-  const handleChange=(e)=>{}
-  const handleBlur=(e)=>{}
-  const handleSubmit=(e)=>{}
+  const handleChange=(e)=>{
+    setForm({
+      ...form,
+      [e.target.name]:[e.target.value]
+    })
+  }
+  const handleBlur=(e)=>{
+  //  Este evento se desencadenara y validara el input cuando se deje de seleccionar
+  //Esto es para validar antes de que se envie la informacion a traves del handle Submit
+  }
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log(form);
+    setForm(initialForm)
+  }
 
   return{
     form,
--- src/assets/Components/ContactForm.jsx
+++ src/assets/Components/ContactForm.jsx
@@ -1,9 +1,66 @@
+import { useForm } from "../../HOOKS/useForm";
 
+const initialForm = {
+  name: "",
+  email: "",
+  subject: "",
+  comments: "",
+};
+const validations = () => {};
 
-const ContactForm=()=>{
-    return(<>
-       <p>Formulariio de Contacto </p>
-    </>)
-}
+const ContactForm = () => {
+  const {
+    form,
+    error,
+    loading,
+    response,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+  } = useForm(initialForm, validations);
 
-export default ContactForm
+  return (
+    <>
+      <h2>Formulariio de Contacto </h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Escribe tu nombre"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={form.name}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Escribe tu email"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={form.email}
+        />
+        <input
+          type="text"
+          name="subject"
+          placeholder="Asunto a tratar"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={form.subject}
+        />
+        <textarea
+          name="comments"
+          cols="50"
+          rows="5"
+          placeholder="Escribe tus comnetarios"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={form.comments }
+        ></textarea>
+
+        <input type="submit" value="Enviar" />
+      </form>
+    </>
+  );
+};
+
+export default ContactForm;
