3c487334b9e2dfb407de059b1f50a4b5443819ad
Dev-Simon20 <jeanpierks6@gmail.com>
2023-11-25 01:18:57 -0500

    Valida los inputs a traves de expresions regulares

--- src/assets/Components/ContactForm.jsx
+++ src/assets/Components/ContactForm.jsx
@@ -7,19 +7,40 @@ const initialForm = {
   comments: "",
 };
 const validations = (form) => {
-  const {name,email,subject,comments}=form;
-  let errors={};
+  //   Destructuramos el form
+  const { name, email, subject, comments } = form;
+  //  creamos el objeto error
+  let errors = {};
+  // creamos las expresiones regulares para poder validar el formulario
+  let regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+  let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+  let regexComments = /^.{1,255}$/;
   if (!name.trim()) {
-      errors.name="El campo nombre es requerido"
+    errors.name = "El campo nombre es requerido";
+  } else if (!regexName.test(name.trim())) {
+    errors.name = "El campo Nombre solo acepta letras y espacios en blanco";
+  }
+  if (!email.trim()) {
+    errors.email = "El campo Email es requerido";
+  } else if (!regexEmail.test(email.trim())) {
+    errors.email = "El campo Email es incorrecto";
+  }
+  if (!subject.trim()) {
+    errors.subject = "El campo Asunto atratar es requerido";
+  }
+  if (!comments.trim()) {
+    errors.comments = "El campo Comment es requerido";
+  } else if (!regexComments.test(comments.trim())) {
+    errors.comments = "El campo Comments no debe exceder los 255 caracteres";
   }
 
   return errors;
 };
 
-let styles={
-    fontWeight:'bold',
-    color:'#dc3545'
-}
+let styles = {
+  fontWeight: "bold",
+  color: "#dc3545",
+};
 
 const ContactForm = () => {
   const {
@@ -44,7 +65,7 @@ const ContactForm = () => {
           onBlur={handleBlur}
           value={form.name}
         />
-        {error.name&&<p style={styles}>{error.name}</p>}
+        {error.name && <p style={styles}>{error.name}</p>}
         <input
           type="email"
           name="email"
@@ -53,7 +74,7 @@ const ContactForm = () => {
           onBlur={handleBlur}
           value={form.email}
         />
-        {error.email&&<p style={styles}>{error.email}</p>}
+        {error.email && <p style={styles}>{error.email}</p>}
         <input
           type="text"
           name="subject"
@@ -62,7 +83,7 @@ const ContactForm = () => {
           onBlur={handleBlur}
           value={form.subject}
         />
-        {error.subject&&<p style={styles}>{error.subject}</p>}
+        {error.subject && <p style={styles}>{error.subject}</p>}
         <textarea
           name="comments"
           cols="50"
@@ -70,9 +91,9 @@ const ContactForm = () => {
           placeholder="Escribe tus comnetarios"
           onChange={handleChange}
           onBlur={handleBlur}
-          value={form.comments }
+          value={form.comments}
         ></textarea>
-        {error.comments&&<p style={styles}>{error.comments}</p>}
+        {error.comments && <p style={styles}>{error.comments}</p>}
 
         <input type="submit" value="Enviar" />
       </form>
